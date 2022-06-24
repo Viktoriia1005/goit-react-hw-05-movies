123413088abcbb40747e73701869bcfe13fd192e
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-06-24 13:45:20 +0300

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -6,51 +6,32 @@ const API_KEY = '76293c6bcb8bbcc89a96d2b767d5c3a3';
 axios.defaults.baseURL = BASE_URL;
 
 export const FetchTrend = async () => {
-  try {
-    const response = await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
+  return response.data.results;
 };
 
 export const FetchDetails = async movie_id => {
-  try {
-    const response = await axios.get(
-      `/movie/${movie_id}?api_key=${API_KEY}&language=en-US`,
-      movie_id
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get(`/movie/${movie_id}?api_key=${API_KEY}`);
+  return response.data;
 };
+
 export const FetchCast = async movie_id => {
-  try {
-    const response = await axios.get(
-      `/movie/${movie_id}/credits?api_key=${API_KEY}&language=en-US`,
-      movie_id
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get(
+    `/movie/${movie_id}/credits?api_key=${API_KEY}`
+  );
+  return response.data.cast;
 };
+
 export const FetchReviews = async movie_id => {
   const response = await axios.get(
     `/movie/${movie_id}/reviews?api_key=${API_KEY}`
   );
-  const reviewDetails = await response.data;
-  return reviewDetails;
+  return response.data.results;
 };
+
 export const FetchSearch = async query => {
-  try {
-    const response = await axios.get(
-      `/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`,
-      query
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get(
+    `/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+  );
+  return response.data;
 };
